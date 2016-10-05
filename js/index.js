7f9ed777460f001e75edf627510a56520c072c4a
kyle <kyjoconn@ucsc.edu>
2016-10-04 21:07:55 -0700

    just need to translate to spanish

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*$(function () {
+
+inSpanish = true;
+
+$("#languageswap").click(function () {
+  console.log("clicked");
+  inSpanish = !inSpanish;
+
+  if (inSpanish) {
+    $("#home").html('casa');
+  } else {
+    $("#home").html('home');
+  };
+
+});
+
+console.log(inSpanish);
+
+
+
+
+
+});*/
